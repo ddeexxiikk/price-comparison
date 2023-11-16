ce86864ad93545a8d21f1d7f604fcb960a6e93ec
Jakub Batycki <jakubbatycki@protonmail.ch>
2023-11-16 14:57:24 +0100

    Skeleton of scraper

--- lib/scraper.py
+++ lib/scraper.py
@@ -1,3 +1,31 @@
 #Plik zawiera funkcje do pobierania danych z internetu
 
 import BeautifulSoup
+
+def pobierz_dane_RTV_EURO_AGD(adres):
+    #Pobranie danych z RTV EURO AGD
+    return
+
+def pobierz_dane_Media_Markt(adres):
+    #Pobranie danych z Media Markt
+    return
+
+def pobierz_dane_Neonet(adres):
+    #Pobranie danych z Neonet
+    return
+
+def pobierz_dane_X_Kom(adres):
+    #Pobranie danych z X-Kom
+    return
+
+def pobierz_dane_Komputronik(adres):
+    #Pobranie danych z Komputronik
+    return
+
+def pobierz_dane_Media_Expert(adres):
+    #Pobranie danych z Media Expert
+    return
+
+def pobranie_wszytskich_danych(adresy):
+    #Uruchomienie wszystkich skryptów pobierających dane
+    return
