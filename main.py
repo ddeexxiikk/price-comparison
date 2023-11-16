dff41ecf29db1022ae02550ade21218dec4fff22
Jakub Batycki <jakubbatycki@protonmail.ch>
2023-11-16 14:53:12 +0100

    Update main'a

--- main.py
+++ main.py
@@ -4,6 +4,33 @@ import lib/scraper.py
 #Główna funkcja main, wywołująca pozostałe funkcje
 def main():
     print("Witaj w Porównywarce cen!")
+    print("Podaj tytuł produktu: ")
+    tytul = input()
+
+    adresy = []
+    print("Podaj adres URL produktu: ")
+    adresy.append(input())
+    
+    while(True):
+        print("Czy chcesz podać kolejny adres? (y/n)")
+        odp = input()
+        if(odp == "y"):
+            print("Podaj adres URL produktu: ")
+            adresy.append(input())
+        elif(odp == "n"):
+            break
+        else:
+            print("Nieprawidłowa odpowiedź")
+
+    print("Pobieram ceny dla Ciebie...")
+
+    #Uruchomienie skryptów i stworzenie/update bazy danych
+
+    print("Porównanie cen czeka na Ciebie w pliku. Miłego Dnia!")
+
+    time.sleep(3)
+    print("Wciśnij dowolny klawisz, aby zamknąć okno")
+    input()
     
 
 #Wywołanie funkcji main
