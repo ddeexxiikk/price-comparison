45ccf37a00eb7e65bc2307807ae0d05983b6cf12
Jakub Batycki <jakubbatycki@protonmail.ch>
2023-11-16 17:41:55 +0100

    Recognize stores and create/update files

--- lib/scripts.py
+++ lib/scripts.py
@@ -1,17 +1,48 @@
 #Plik przechowujący funkcję sprawdzające link i tworzące pliki bazy danych
+import scraper
+import time
+
 
 def sprawdz_sklep(adres):
-    if(adres.find("rtveuroagd") != -1):
+    if(adres.find("euro.com.pl") != -1):
         return "RTV EURO AGD"
-    elif(adres.find("komputronik") != -1):
+    elif(adres.find("komputronik.pl") != -1):
         return "Komputronik"
-    '''
-    elif(adres.find("mediamarkt") != -1):
-        return "Media Markt"
-    elif(adres.find("neonet") != -1):
-        return "Neonet"
-    elif(adres.find("x-kom") != -1):
-        return "X-Kom"
-    '''
     else:
         return None
+    
+    '''
+    elif(adres.find("mediamarkt.pl") != -1):
+        return "Media Markt"
+    elif(adres.find("neonet.pl") != -1):
+        return "Neonet"
+    elif(adres.find("x-kom.pl") != -1):
+        return "x-kom"
+    elif(adres.find("mediaexpert.pl") != -1):
+        return "Media Expert"
+    elif(adres.find("avans.pl") != -1):
+        return "Avans"
+    elif(adres.find("allegro.pl") != -1):
+        return "Allegro"
+    '''
+
+def stworz_plik_bazy_danych(tytul):
+    #Stworz plik CSV z tytulem i data w folderze Database
+    nazwa_pliku = "Database/" + tytul + ".csv"
+    plik = open(nazwa_pliku, "w")
+    #Dodaj naglowki do pliku: w pierwszej linii data wraz z godziną, w drugiej sklep,cena 
+    plik.write("Data: " + time.strftime("%d/%m %H:%M") + "\n")
+    plik.write("Sklep,Cena\n")
+    plik.close()
+
+    return plik
+
+def update_pliku_bazy_danych(tytul, sklep, cena):
+    #Otworz plik CSV z tytulem w folderze Database
+    nazwa_pliku = "Database/" + tytul + ".csv"
+    plik = open(nazwa_pliku, "a")
+    #Dodaj do pliku sklep i cene
+    plik.write(sklep + "," + cena + "\n")
+    plik.close()
+
+    return plik
